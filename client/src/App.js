f497a06e77882f0ba8aeb4b094ea051713da65d6
spencer-howell <spencer.howell@outlook.com>
2020-01-22 08:43:57 -0500

    Add client folder back to git repo

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/itemModal";
+import { Provider } from "react-redux";
+import { Container } from "reactstrap";
+import store from "./store";
+import "./App.css";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
