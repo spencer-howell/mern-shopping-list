29d057f26ec7687182cfde9ccb4602d8a6956deb
Spencer Howell <spencer.howell@Outlook.com>
2020-01-22 09:08:14 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb://snk:Password1@ds061767.mlab.com:61767/mern_shopping"
+  mongoURI: "mongodb://<username>:<Password>@ds061767.mlab.com:61767/mern_shopping"
 };
